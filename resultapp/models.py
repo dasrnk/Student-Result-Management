8028d6921780b9f3abac28232008bf0d72ec0d9a
Raneek Das <dasraneek@gmail.com>
2026-04-28 01:29:56 +0530

    second commit

---
+++ resultapp/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+
+class Class(models.Model): 
+    class_name = models.CharField(max_length=50)
+    class_numeric = models.IntegerField()
+    section = models.CharField(max_length=20)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    updation_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.class_name} - {self.section}"
+
+class Subject(models.Model): 
+    subject_name = models.CharField(max_length=100)
+    subject_code = models.CharField(max_length=20)
+    section = models.CharField(max_length=20)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    updation_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.subject_name} - {self.subject_code}"
+
+class Student(models.Model):
+    GENDER_CHOICES = (
+        ('Male','Male'),
+        ('Female','Female'),
+    )
+
+    student_name = models.CharField(max_length=100)
+    roll_id = models.CharField(max_length=30)
+    email_id = models.EmailField(max_length=50)
+    gender = models.CharField(max_length=10,choices=GENDER_CHOICES)
+    dob = models.CharField(max_length=100)
+    student_class = models.ForeignKey(Class,on_delete=models.SET_NULL,null=True)
+    reg_date = models.DateTimeField(auto_now_add=True)
+    updation_date = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return self.student_name
+    
+class SubjectCombination(models.Model):
+    student_class = models.ForeignKey(Class,on_delete=models.SET_NULL,null=True)
+    Subject = models.ForeignKey(Subject,on_delete=models.SET_NULL,null=True)
+    status = models.IntegerField(default=1)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    updation_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.student_class} - {self.Subject}" 
+    
+class Result(models.Model):
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    student_class = models.ForeignKey(Class,on_delete=models.SET_NULL,null=True)
+    subject = models.ForeignKey(Subject,on_delete=models.SET_NULL,null=True)
+    marks = models.IntegerField()
+    posting_date = models.DateTimeField(auto_now_add=True)
+    updation_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.student} - {self.Subject} - {self.marks}" 
+    
+class Notice(models.Model):
+    title = models.CharField(max_length=100)
+    detail = models.TextField(max_length=100)
+    posting_date = models.DateTimeField(auto_now_add=True)
+    updation_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
---
+++ resultapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate , login
+from django.contrib import messages
+def index(request):
+    return render(request,'index.html')
+
+def admin_login(request):
+    if request.user.is_authenticated:
+        return redirect('admin_dashboard')
+    error = None
+    if request.method == 'POST':
+       username = request.POST['username']
+       password = request.POST['password']
+       user = authenticate(request,username=username,password=password)
+
+       if user is not None and user.is_superuser:
+           login(request, user)
+           return redirect('admin_dashboard')
+       else:
+           error = "Invalid credentials or not authorized."
+    return render(request,'admin_login.html',locals())
+
+def admin_dashboard(request):
+    return render(request,'admin_dashboard.html')
+
+def create_class(request):
+    return render(request,'create_class.html')
+
